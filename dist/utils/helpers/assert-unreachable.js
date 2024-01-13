0fb1d2a65d1c92d1853b79b00493cf979048c444
Mayer Starkman <mayerstrk@gmail.com>
2024-01-13 17:45:36 +0000

    remove dist

--- dist/utils/helpers/assert-unreachable.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function assertUnreachable(x, message) {
-    throw new Error(`${message}: ${x}`);
-}
-exports.default = assertUnreachable;
-//# sourceMappingURL=assert-unreachable.js.map
