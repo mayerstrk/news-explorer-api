d5f6f113d013f5f226e058b2f3fc6d477e6412d7
Mayer Starkman <mayerstrk@gmail.com>
2024-01-13 21:39:31 +0000

    fix(validate-token): remove check for 3 keys

--- src/utils/helpers/is-request-user.ts
+++ src/utils/helpers/is-request-user.ts
@@ -4,11 +4,7 @@ function isRequestUser(value: unknown): value is RequestUser {
 	if (typeof value !== 'object' || value === null) return false;
 
 	const keys = Object.keys(value);
-	return (
-		keys.length === 3 &&
-		keys[0] === '_id' &&
-		typeof (value as any)._id === 'string'
-	);
+	return keys[0] === '_id' && typeof (value as any)._id === 'string';
 }
 
 export { isRequestUser };
