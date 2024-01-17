8f6fb076737954232bd6fe2073b3e050c1a1ffc3
Mayer Starkman <mayerstrk@gmail.com>
2024-01-17 19:47:27 +0000

    feat: add diferent limits for prod and dev environments

--- src/middleware/rate-limiter.ts
+++ src/middleware/rate-limiter.ts
@@ -1,8 +1,9 @@
 import rateLimit from 'express-rate-limit';
+import { env } from '../environment-config';
 
 const rateLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
-	max: 300,
+	max: env.NODE_ENV === 'production' ? 750 : 2000,
 	standardHeaders: true,
 	legacyHeaders: true,
 });
